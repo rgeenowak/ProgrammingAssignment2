c217dfeb2e890665a652548cad0d5300f834d68f
RNowak <rnowak@ihv.umaryland.edu>
2018-10-10 22:34:26 -0400

    modified cachematrix.R to include code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## 10-10-18
+## R_course assignment 2
+## Generate functions that cache the inverse of a matrix
+## creates a matrix object and illustrates lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function()x
+  seti<-function(inv)i<<-inv
+  geti<-function()m
+  list (set=set, get=get, seti=seti, geti=geti)
 }
 
-
-## Write a short comment describing this function
+## function generates the inverse of the matrix cached by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i<-x$geti()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data<-x$get()
+  i<-solve(data)
+  x$seti(i)
+  i      
 }
